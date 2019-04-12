b63c6b58afebf49a78a542786952b4956728d075
Julio C. Restrepo <julio.restrepor@udea.edu.co>
2019-04-11 21:42:55 -0500

    Add files via upload
    Particle content definitions. Electric charge, mass and spin.

---
+++ p_definition.h
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct particle
+{
+  float m;
+  float q;
+  float s;
+  float E;
+  };
+
+particle ep = {0.0005, +1, 0.5, 0};
+particle em = {0.0005, -1, 0.5};
+particle mup = {0.105, +1, 0.5};
+particle mum = {0.105, -1, 0.5};
+particle taup = {1.777, +1, 0.5};
+particle taum = {1.777, -1, 0.5};
+particle h = {125, 0, 0};
+particle u = {0.003, 2/3., 0.5};
+particle ub = {0.003, -2/3., 0.5};
+particle d = {0.007, -1/3., 0.5};
+particle db = {0.007, 1/3., 0.5};
+particle s = {0.12, -1/3., 0.5};
+particle sb = {0.12, 1/3., 0.5};
+particle c = {1.2, 2/3., 0.5};
+particle cb = {1.2, -2/3., 0.5};
+particle t = {174, 2/3., 0.5};
+particle tb = {174, -2/3., 0.5};
+particle b = {4.3, -1/3., 0.5};
+particle bb = {4.3, 1/3., 0.5};
+particle ve = {0, 0, 0.5};
+particle vu = {0, 0, 0.5};
+particle vt = {0, 0, 0.5};
+particle a = {0, 0, 1};
+particle g = {0, 0, 1};
+particle wp = {0.0804, +1, 1};
+particle wm = {0.0804, -1, 1};
+particle z = {0.0919, 0, 1};
