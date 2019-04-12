688e2d619e97cb122b7d8430c4e92bed7302bfc3
Julio C. Restrepo <julio.restrepor@udea.edu.co>
2019-04-11 21:59:16 -0500

    Add files via upload

--- p_definition.h
+++ p_definition.h
@@ -1,13 +1,13 @@
 struct particle
 {
-  float m;
-  float q;
-  float s;
-  float E;
+  float m;           //Particle mass
+  float q;           //Electric charge
+  float s;           //Spin
+  float E;           //Energy
   };
 
 //Masses in GeV/c^2, electric charges in e units
-
+//Leptons
 particle ep = {5e-4, +1, 0.5, 0};
 particle em = {5e-4, -1, 0.5, 0};
 
@@ -17,8 +17,10 @@ particle mum = {0.105, -1, 0.5, 0};
 particle taup = {1.777, +1, 0.5, 0};
 particle taum = {1.777, -1, 0.5, 0};
 
+//Higgs
 particle h = {125, 0, 0, 0};
 
+//Quarks
 particle u = {3e-2, 2/3., 0.5, 0};
 particle ub = {3e-2, -2/3., 0.5, 0};
 
@@ -37,13 +39,15 @@ particle tb = {174, -2/3., 0.5, 0};
 particle b = {4.3, -1/3., 0.5, 0};
 particle bb = {4.3, 1/3., 0.5, 0};
 
+//Neutrinos
 particle ve = {0, 0, 0.5, 0};
 particle vu = {0, 0, 0.5, 0};
 particle vt = {0, 0, 0.5, 0};
 
-particle a = {0, 0, 1, 0};
-particle g = {0, 0, 1, 0};
+//Vectorial bosons
+particle a = {0, 0, 1, 0}; //Photon
+particle g = {0, 0, 1, 0}; //Gluon
 
-particle wp = {0.0804, +1, 1, 0};
-particle wm = {0.0804, -1, 1, 0};
-particle z = {0.0919, 0, 1, 0};
+particle wp = {0.0804, +1, 1, 0}; //W+
+particle wm = {0.0804, -1, 1, 0}; //W-
+particle z = {0.0919, 0, 1, 0};   //Z
