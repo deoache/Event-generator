cd6299c778b611da9445546514b80702c5985dfe
Julio C. Restrepo <julio.restrepor@udea.edu.co>
2019-04-13 19:20:14 -0500

    Add files via upload

--- states_constructor.h
+++ states_constructor.h
@@ -4,21 +4,36 @@
 
 std::string state1;
 std::string state2;
-std::vector<std::string> state_out;
-std::vector<std::string> state_in;
-int counter{0};
-int Nparticles;
+std::vector<std::string> state_out; //Vector with the names of particles in the final state 
+std::vector<std::string> state_in;  //Vector with the names of particles in the initial state
+int counter{0};                     //Counter for the number of particles
+int Nparticles;                    //Number of particles in the final state
 
 void states(){
-   
+
+  std::cout << "***** Event generator *****\n";
+  std::cout << "Particles are defined as:\n";
+  
+  std::cout << "Charged leptons and antileptons are: e- e+, mu- mu+, tau- tau+\n";
+
+  std::cout << "Quarks and antiquarks are: u u~, d d~, c c~, s s~, t t~, b b~\n";
+
+  std::cout << "Electronic, muonic and tauonic neutrinos are respectively: ve, vu, vt\n";
+
+  std::cout << "Spin 1 bosons are: a, g, w+, w-, z\n";
+
+  std::cout << "Higgs bosson is: h\n";
+
+  //Enter the states
   std::cout << "Initial state\n";
   std::getline(std::cin, state1);
   std::cout << "Final state\n";
   std::getline(std::cin, state2);
 
-  int len1 = state1.size();
-  int len2 = state2.size();
-  
+  int len1 = state1.size();   //Number of characteres in the initial state
+  int len2 = state2.size();   //Number of characteres in the final state
+
+  //Counter for the particles in the final state
   for(int i = 0; i < len2; i++){
     if(state2[i] == ' '){
       counter += 1;
@@ -32,33 +47,36 @@ void states(){
   else{
     Nparticles = counter + 1;
     }
-  
+
+  //Resize the state vectors
   state_out.resize(Nparticles);
   state_in.resize(2);
 
-  char state_i[2]{'s'};
+  //Contruct the vector with the labels of particles in the initial state
   int count{0};
   for(int i = 0; i < 2; i++){
-    
     for(int ii = count; ii < len1; ii++){
+      
       if(state1[ii] == ' '){
 	count += 1;
 	break;
       }
+      
       state_in[i] += state1[ii];
       count += 1;
 }
   }
 
-  char state_f[len2-counter]{'s'};
+  //Contruct the vector with the labels of particles in the final state
   count = 0;
   for(int i = 0; i < Nparticles; i++){
-    
     for(int ii = count; ii < len2; ii++){
+      
       if(state2[ii] == ' '){
 	count += 1;
 	break;
       }
+      
       state_out[i] += state2[ii];
       count += 1;
 }
